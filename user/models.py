c968721b67219686729a8fc7ca52ad42c22d8894
Bogyi95 <krzysiek.bogusz@onet.pl>
2023-04-04 17:34:01 +0200

    naprawione defaultowe zdjecia uzytkownikow

--- user/models.py
+++ user/models.py
@@ -6,7 +6,7 @@ class Profile(models.Model):
     staff = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     address = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=20, null=True)
-    image = models.ImageField(default='media/avatar.jpg', upload_to='Profile_Images')       #NAPRAWIC TE AVATARY (NIE MOGE OGARNAC
+    image = models.ImageField(default='/avatar.jpg', upload_to='Profile_Images')       #NAPRAWIC TE AVATARY (NIE MOGE OGARNAC
                                                                                                 #O CO CHODZI Z TYMI ICONAMI AVATAROW)
 
     def __str__(self):
