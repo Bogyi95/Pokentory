99b0937b2459ca490ad2328637e47a2359b93763
Bogyi95 <krzysiek.bogusz@onet.pl>
2023-04-08 18:52:23 +0200

    Configuring product-user page

--- inventorymanagement/settings.py
+++ inventorymanagement/settings.py
@@ -120,20 +120,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATTICFILES_DIR = [
-    (BASE_DIR, "static"),
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
 ]
 
 STATIC_ROOT = (BASE_DIR/"asert/")
 
-MEDIA_ROOT = (BASE_DIR/'media')
+MEDIA_ROOT = (BASE_DIR/"media/")
 
 MEDIA_URL = '/media/'
 
